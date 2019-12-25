ea0a0b665d0af1578f16923c3836671ea633b79e
irfanabdnst <irfan.abdillah@gmail.com>
2019-12-25 23:19:41 +0700

    Make the sign-up web service endpoint public

--- src/main/java/com/vesnadev/app/ws/service/UserService.java
+++ src/main/java/com/vesnadev/app/ws/service/UserService.java
@@ -1,8 +1,9 @@
 package com.vesnadev.app.ws.service;
 
 import com.vesnadev.app.ws.shared.dto.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 
     UserDto createUser(UserDto user);
 
--- src/main/java/com/vesnadev/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/vesnadev/app/ws/service/impl/UserServiceImpl.java
@@ -7,6 +7,8 @@ import com.vesnadev.app.ws.shared.Utils;
 import com.vesnadev.app.ws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -41,4 +43,8 @@ public class UserServiceImpl implements UserService {
         return returnValue;
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
 }
