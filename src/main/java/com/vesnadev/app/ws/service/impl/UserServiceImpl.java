2471f696cacda13c865f88b7f3017422bd61481b
irfanabdnst <irfan.abdillah@gmail.com>
2019-12-25 22:23:41 +0700

    Generate userId

--- src/main/java/com/vesnadev/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/vesnadev/app/ws/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.vesnadev.app.ws.service.impl;
 import com.vesnadev.app.ws.UserRepository;
 import com.vesnadev.app.ws.io.entity.UserEntity;
 import com.vesnadev.app.ws.service.UserService;
+import com.vesnadev.app.ws.shared.Utils;
 import com.vesnadev.app.ws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
         if (userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Email already exists");
@@ -21,8 +25,9 @@ public class UserServiceImpl implements UserService {
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
+        String publicUserId = utils.generateUserId(30);
         userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testUserId");
+        userEntity.setUserId(publicUserId);
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
 
