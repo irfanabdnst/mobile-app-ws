14674ade02282c23bd8c81f84765b9538d01341d
irfanabdnst <irfan.abdillah@gmail.com>
2019-12-31 21:19:55 +0700

    Implementing error message in UserController

--- src/main/java/com/vesnadev/app/ws/ui/controller/UserController.java
+++ src/main/java/com/vesnadev/app/ws/ui/controller/UserController.java
@@ -3,6 +3,7 @@ package com.vesnadev.app.ws.ui.controller;
 import com.vesnadev.app.ws.service.UserService;
 import com.vesnadev.app.ws.shared.dto.UserDto;
 import com.vesnadev.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.vesnadev.app.ws.ui.model.response.ErrorMessages;
 import com.vesnadev.app.ws.ui.model.response.UserRest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,8 +30,10 @@ public class UserController {
     }
 
     @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws Exception {
         UserRest returnValue = new UserRest();
+
+        if (userDetails.getFirstName().isEmpty()) throw new Exception(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
 
         UserDto userDto = new UserDto();
         BeanUtils.copyProperties(userDetails, userDto);
