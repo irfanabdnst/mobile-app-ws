2dc1cbcdf921b1d000d88606f9a9dd1683c6dc18
irfanabdnst <irfan.abdillah@gmail.com>
2020-01-03 22:43:46 +0700

    Update AddressRepository.java

--- src/main/java/com/vesnadev/app/ws/io/repositories/AddressRepository.java
+++ src/main/java/com/vesnadev/app/ws/io/repositories/AddressRepository.java
@@ -5,9 +5,11 @@ import com.vesnadev.app.ws.io.entity.UserEntity;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends PagingAndSortingRepository<AddressEntity, Long> {
 
-    AddressEntity findByUserDetails(UserEntity userDetails);
+    List<AddressEntity> findByUserDetails(UserEntity userDetails);
 
 }
